24f741c195421f7c9ba252513117396d6cd9ba6c
Han <tanghan1995@gmail.com>
2020-12-13 15:07:19 +0800

    Add files via upload

---
+++ 2020.12.13/WeeklyContest219/PartitioningIntoMinimumNumberOfDeci_BinaryNumbers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 13 11:56:29 2020
+
+@author: TH
+"""
+class Solution:
+    def minPartitions(self, n):
+        return max([int(i) for i in n])
